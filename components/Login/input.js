5dafc53a4422d7ee6df32541063a0a04a618f883
Pasindu Madusanka <pasindusenerath@gmail.com>
2019-05-09 09:38:54 +0530

    Login Function completed

--- components/Login/input.js
+++ components/Login/input.js
@@ -1,49 +1,46 @@
 import React from 'react';
 import {View,StyleSheet,Text,TextInput} from 'react-native';
 
-const Input = ({label,value, onChangeText,placeholder,secureTextEntry}) => {
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
     return (
-        <View style={styles.container}>
+      <View style={styles.container}>
         <Text style={styles.label}>{ label }</Text>
         <TextInput
-        autoCorrect={false} 
-        onChangeText={onChangeText}
-        placeholder={placeholder}
-        style={styles.input}
-        secureTextEntry={secureTextEntry}
-        value={value}
+          autoCorrect={false}
+          onChangeText={onChangeText}
+          placeholder={placeholder}
+          style={styles.input}
+          secureTextEntry={secureTextEntry}
+          value={value}
         />
-        </View>
+      </View>
     )
-}
-
-const styles= StyleSheet.create({
-    container:{
-        marginTop:10,
-        width:'100%',
-        borderColor:'#eee',
-        borderBottomWidth:2,
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      marginTop: 10,
+      width: '100%',
+      borderColor: '#eee',
+      borderBottomWidth: 2,
     },
-
-label:{
-    padding:5,
-    paddingBottom:0,
-    color:'#333',
-    fontSize: 17,
-    fontWeight:'700',
-    width: '100%',
-
-
-},
-input:{
-    paddingRight:5,
-    paddingLeft:5,
-    paddingBottom:2,
-    color: '#333',
-    fontSize:18,
-    fontWeight:'700',
-    width:'100%',
-}
-});
-
-export {Input};
+    label: {
+      padding: 5,
+      paddingBottom: 0,
+      color: '#333',
+      fontSize: 17,
+      fontWeight: '700',
+      width: '100%',
+    },
+    input: {
+      paddingRight: 5,
+      paddingLeft: 5,
+      paddingBottom: 2,
+      color: '#333',
+      fontSize: 18,
+      width: '100%',
+    }
+  });
+  
+  export { Input };
+  
