664bc0b1edac9ba6497267b2b87bbd1644dfe941
AsithaIndrajith <asithaindrajithk9@gmail.com>
2019-05-19 07:07:33 +0530

    add comments

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+/*
+*1. React Navigation 3.0 has a number of breaking changes including an explicit app
+*   container required for the root navigator
+*2. In the past, any navigator could act as the navigation container at the
+*   top-level of your app because they were all wrapped in “navigation containers”.
+*3.
+* */
 import {
   createStackNavigator,
   createAppContainer
 } from 'react-navigation';
+
+/*Import screens*/
 import Login from './src/screens/Login/Login'
 import UserDashboard from './src/screens/UserDashboard/UserDashboard'
 import PHIDashboard from './src/screens/PHIDashboard/PHIDashboard'
@@ -21,6 +31,10 @@ const RootStack = createStackNavigator({
 
 });
 
+/*
+* The navigation container, now known as an app container, is a higher-order-component that maintains the navigation state
+* of your app and handles interacting with the outside world to turn linking events into navigation actions and so on
+* */
 const App = createAppContainer(RootStack);
 
 export default App;
